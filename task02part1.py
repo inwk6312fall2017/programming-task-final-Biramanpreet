239c08c66a7ce9f640f6c13e8602b1e77044bec8
biramanpreetsingh <biramanpreet.singh@dal.ca>
2017-10-20 14:57:06 -0400

    last days data

--- task02part1.py
+++ task02part1.py
@@ -1,18 +1,17 @@
 from weather import Weather
-weather = Weather
+weather = Weather()
 
-location = weather.lookup_by_location('Halifax')
-condition = location.condition('Halifax')
-print (condition['text'])
-forecast = location.forecast()
-hightemp=[]
-lowtemp=[]
-print("current weather is ", condition['text'])
-print("current temperature ",condition['temp'])
-print("next 5 days")
-for forecasts in location.forecast():
-        hightemp = forecasts['high']
-        lowtemp = forecasts['low']
-        rain= forecast['text']
-        if forecasts['text']== "Rain":
-                print("It will rain on",forecasts['day'])
+area=weather.lookup_by_location('halifax')
+cond=area.condition()
+days=[]
+x=0
+for forecasts in area.forecast():
+    if x<5:
+        newlist=[]
+        newlist.append(forecasts['text'])
+        newlist.append(forecasts['date'])
+        newlist.append(forecasts['high'])
+        newlist.append(forecasts['low'])
+        days.append(newlist)
+        x=x+1
+print (days)
