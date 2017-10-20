f700433271149ff93b9c689db2843897e13e3986
biramanpreetsingh <biramanpreet.singh@dal.ca>
2017-10-20 14:08:56 -0400

    This Commit is for Task1 Successful comparison of all three books

--- task01.py
+++ task01.py
@@ -1,4 +1,4 @@
-book1=open('Book1.txt','r')
+ book1=open('Book1.txt','r')
 book2=open('Book2.txt','r')
 book3=open('Book3.txt','r')
 longest=0
@@ -12,7 +12,7 @@ for word in book2.read().split():
         longest2 = len(word)
         longest_wordBook2 =longest2
 longest3=0
-for word in book2.read().split():
+for word in book3.read().split():
     if len(word) > longest3:
         longest3 = len(word)
         longest_wordBook3 =longest3
@@ -27,6 +27,7 @@ elif longest_wordBook2 > longest_wordBook3:
     else:
        print("The longest word is in Book1 ie. ",longest_wordBook1)    
 
-    
+else:
+      print("The longest word is in Book3 ie. ",longest_wordBook3)    
    
 
